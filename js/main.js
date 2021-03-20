98f8c27ada214469c253b9b235bd80d22dcbdb10
Anna <a.s.antoniuk06@gmail.com>
2021-03-20 18:38:19 +0200

    work in js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
-let money = ("Ваш бюджет на месяц?", ' ');
-let time = ("Ввудите дату в формате YYYY-MM-DD", '');
+let money = prompt ("Ваш бюджет на месяц?", ' ');
+let time = prompt ("Ввудите дату в формате YYYY-MM-DD", '');
 
 let appData = {
     budjet: money,
@@ -11,13 +11,39 @@ let appData = {
     income: [],
     saving: false
 };
-let a1 = ("Введите обязательную статью расходов в этом месяце", '');
-let a2 = ("Во сколько обойдется?", '');
-let a3 = ("Введите обязательную статью расходов в этом месяце", '');
-let a4 = ("Во сколько обойдется?", '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-console.log(appData.expenses)
-alert(appData.budjet/30);
+
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt ("Введите обязательную статью расходов в этом месяце", '');
+    let b = prompt ("Во сколько обойдется?", '');
+
+    if ((tipeof(a)) === 'string' && (tipeof(a)) != null && (tipeof(b)) != null  && a != '' && b != '' && a.lenght < 50) {
+
+        console.log(done);
+        appData.expenses[a] = b; // добавляем ключ(свойство) в обьект через квадратные скобки и его значение 
+    } else {
+            console.log ("bad resalt");
+            i--;
+        }
+
+    
+};
+
+appData.moneyPerDay = appData.budjet / 30;
+alert("Ежедневный бюджет:" + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log ("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Это высокий уровень достатка!");
+} else {
+    console.log ("Произошла ошибка");
+}
+
+
+
+
 
