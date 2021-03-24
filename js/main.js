86dbce48e91020079c57fedf5ab8e72cfb4d97b2
Anna <a.s.antoniuk06@gmail.com>
2021-03-24 19:20:41 +0200

    add function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 'use strict'
+let money;
+let time;
 
-let money = prompt ("Ваш бюджет на месяц?", ' ');
-let time = prompt ("Ввудите дату в формате YYYY-MM-DD", '');
+function start () {
+    money = +prompt ("Ваш бюджет на месяц?", ' ');
+    time = prompt ("Ввудите дату в формате YYYY-MM-DD", '');
+
+    while (isNaN(money) || money == '' || money == null) { //проверяем если ввели не цыфры, пустую строку или нажали отмена цикл будет продолжаться, т.е вопрос останется пока не введут цыфру
+        money = +prompt ("Ваш бюджет на месяц?", ' ');
+    }
+}
+start();
+
 
 let appData = {
     budjet: money,
@@ -14,36 +24,68 @@ let appData = {
 
 
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt ("Введите обязательную статью расходов в этом месяце", '');
-    let b = prompt ("Во сколько обойдется?", '');
-
-    if ((tipeof(a)) === 'string' && (tipeof(a)) != null && (tipeof(b)) != null  && a != '' && b != '' && a.lenght < 50) {
-
-        console.log(done);
-        appData.expenses[a] = b; // добавляем ключ(свойство) в обьект через квадратные скобки и его значение 
-    } else {
-            console.log ("bad resalt");
-            i--;
+function chooseExpenses () {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется?", '');
+        
+        if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
+           && a != '' && b != '' && a.lenght < 50) {
+               console.log("done");
+               appData.expenses[a] = b;
+        } else {
+            i = i-1;
         }
+    }
 
-    
+}
+chooseExpenses();
+
+
+function detectDayBudget () {
+    appData.moneyPerDay = (appData.budjet / 30).toFixed();
+    alert("Ежедневный бюджет:" + appData.moneyPerDay);
+
 };
+detectDayBudget();
 
-appData.moneyPerDay = appData.budjet / 30;
-alert("Ежедневный бюджет:" + appData.moneyPerDay);
+function detectLevel () {
+    if (appData.moneyPerDay < 100) {
+        console.log ("Это минимальный уровень достатка!");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log ("Это средний уровень достатка!");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log ("Это высокий уровень достатка!");
+    } else {
+        console.log ("Произошла ошибка");
+    }
 
-if (appData.moneyPerDay < 100) {
-    console.log ("Это минимальный уровень достатка!");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log ("Это средний уровень достатка!");
-} else if (appData.moneyPerDay > 2000) {
-    console.log ("Это высокий уровень достатка!");
-} else {
-    console.log ("Произошла ошибка");
+}
+detectLevel ();
+
+
+function checkSavings () {
+    if (appData.saving == true){
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save/100/12*percent;
+        alert("Доход в месяц с вашего дупозита:" + appData.monthIncome);   
+    }
 }
 
+checkSavings ();
+
+function chooseOptExpenses() {                             // Функция для определения необязательных расходов
+
+    for (let i = 1; i <= 3; i++) {
+        let questionOptExpenses = prompt("Статья необязательных расходов?");
+        appData.optionalExpenses[i] = questionOptExpenses;
+        console.log(appData.optionalExpenses);
+    }
 
 
+}
+chooseOptExpenses();
 
 
